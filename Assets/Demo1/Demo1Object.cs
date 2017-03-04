bb3929c91911c4a9257fc2183a31371156a6aec5
luke2 <r15yokote@gmail.com>
2017-03-04 15:44:53 +0900

    fix transform async error

--- Assets/Demo1/Demo1Object.cs
+++ Assets/Demo1/Demo1Object.cs
@@ -5,16 +5,20 @@ using AsyncManagement;
 
 public class Demo1Object : MonoBehaviour {
   [SerializeField] Mesh[] meshes;
-  private Quaternion rot;
+  private Quaternion dRot;
+	private Quaternion rot;
 
-  void Start() {
+	void Start() {
+    rot = Quaternion.identity;
+
     AsyncLodFilter filter = GetComponent<AsyncLodFilter>();
 
     filter.OnAsyncUpdate += (dt) => {
-      rot = Quaternion.AngleAxis(30f * dt, Vector3.forward);
+      dRot = Quaternion.AngleAxis(30f * dt, Vector3.forward);
+      rot *= dRot;
     };
     filter.OnPostUpdate += (dt) => {
-      transform.rotation *= rot;
+      transform.rotation = rot;
     };
     filter.OnLevelChanged += (level) => {
       MeshFilter mf = GetComponent<MeshFilter>();
